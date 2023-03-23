85b7db5d97faeec1ca0efe5bc94bce1412924d35
NukeTurtle <estiglic@edocuments.co.uk>
2023-03-23 02:28:50 +0000

    small styling changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,14 +8,17 @@ function Footer() {
                 <a href="mailto:stiglic.elvis@gmail.com?subject=Let's collab (from Portfolio)">
                     <i className="fa-regular fa-envelope"></i>
                 </a>
-                <a href="">
+                <a href="https://github.com/NukeTurtle">
                     <i className="fab fa-github"></i>
                 </a>
-                <a href="">
+                <a href="https://www.instagram.com/nuketurtle/">
                     <i className="fab fa-instagram"></i>
                 </a>
+                <a href="https://www.linkedin.com/in/elvisstiglic/">
+                    <i className="fab fa-linkedin"></i>
+                </a>
             </div>
-            <p>Created with ❤️ and ☕ by Elvis</p>
+            <p>Created with ❤️ and ☕ by Elvis Stiglic</p>
         </footer>
     )
 }
